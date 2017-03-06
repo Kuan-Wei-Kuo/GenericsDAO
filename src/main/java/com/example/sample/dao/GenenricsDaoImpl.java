b964d59ac446a549353728aa81441dc44cdd28c4
郭冠緯(Andy Kuo) <1609007@iisigroup.com>
2017-03-06 18:05:35 +0800

    add hibernate config

--- src/main/java/com/example/sample/dao/GenenricsDaoImpl.java
+++ src/main/java/com/example/sample/dao/GenenricsDaoImpl.java
@@ -3,17 +3,36 @@ package com.example.sample.dao;
 import java.awt.List;
 import java.util.ArrayList;
 
+import org.hibernate.Session;
+
+import com.example.sample.hibernate.HibernateConfig;
+
 public class GenenricsDaoImpl<T> implements GenenricsDao<T>{
 
 	private T entity;
+	
+	private Session session;
+	
+	public GenenricsDaoImpl() {
+		session = HibernateConfig.getSessionFactory().openSession();
+	}
 	
 	public T getEntity() {
 		return entity;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public ArrayList<T> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		session.beginTransaction();
+		
+		ArrayList<T> resultList = (ArrayList<T>) session.createQuery("from Student").list();
+		
+		for(T t : resultList) {
+			System.out.println(t);
+		}
+		
+		return resultList;
 	}
 
 	@Override
--- src/main/java/com/example/sample/hibernate/HibernateConfig.java
+++ src/main/java/com/example/sample/hibernate/HibernateConfig.java
@@ -1,5 +1,25 @@
 package com.example.sample.hibernate;
 
-public class HibernateConfig {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class HibernateConfig {
+ 
+	private static SessionFactory sessionFactory;
+	
+	static {
+		try {
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+		} catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public static void shutdown() {
+        getSessionFactory().close();
+    }
 }
